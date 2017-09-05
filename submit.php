d7f5ca868b330239d078874ffde4cdfa73e8f48a
ajith <ajith.kjames3@gmail.com>
2017-09-05 22:02:35 +0530

    changes before the launch day

--- submit.php
+++ submit.php
@@ -1,14 +1,31 @@
+           
+   
 <?php 
 
-    $to = "d9605408050@gmail.com"; 
+    $to = "ajith.kjames3@gmail.com"; 
     $from = $_POST['email']; 
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
+    $phone = $_POST['phone'];
+    $subject = "site enqury";
     $message = $_POST['message'];
-    
+    $header = 'From: NewEnquiry <' .$from.'>'. "\r\n";
 
-    mail($to,$subject,$message,$from);
-    echo "Email sent!";
+
+
+
+	if(isset($_POST['email']) && isset($_POST['phone'])) {
+	    $data = 'Email from : '.$_POST['email'] . "\n" . "phone :" . $_POST['phone'] . "\n". "name :" . $_POST['name'] . "\n". "message :" . $_POST['message'] . "\n". "\n";
+	    $ret = file_put_contents('queries.txt', $data, FILE_APPEND | LOCK_EX);
+	    
+	}
+	else {
+	   die('no post data to process');
+	}
+    mail($to,$subject,$message,$header);
+   	echo "<script>
+	alert('Email has been sent!');
+	window.location.href='contact.html';
+	</script>";
 
 ?>
            
